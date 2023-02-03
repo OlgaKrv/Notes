de9ac12db7f03c07b88c20d0990a9b9101e67eac
Ola <elina.korotkova@internet.ru>
2023-02-03 09:33:05 +0500

    add blankTitleMessage

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <div id="app">
+  <div id="app" class="project-wrapper">
+    <div class="project-title">
+      <!-- blank title message -->
+      <transition name="message-animation">
+        <message v-if="isNoteTitleEmpty" :blankTitleMessage="blankTitleMessage"/>
+      </transition>
+
+    </div>
   </div>
 </template>
 
 <script>
+import message from '@/components/Message'
 
 export default {
-  name: 'App',
   components: {
+    message
   },
-};
+  data() {
+    return{
+      title: 'Блокнот',
+      isNoteTitleEmpty: false,
+      blankTitleMessage: ""
+    }
+  }
+}
 
 </script>
 
@@ -21,5 +36,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+
+.message-animation-enter-active {
+  animation: message-animation-in .5s ease-out both;
+}
+.message-animation-leave-active {
+  animation: message-animation-in .5s reverse ease-in both;
+}
+@keyframes message-animation-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
---
+++ src/components/Message.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="blank-title-message">
+    <p>Предупреждение - {{ blankTitleMessage }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      blankTitleMessage: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.blank-title-message {
+  text-align: center;
+  padding: 20px;
+}
+p {
+  color: #ba3838;
+}
+</style>
